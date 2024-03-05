dd97c91a3fdcb939da74b61fee9e81223843c07f
Arthur Sena <sena.arthur698@gmail.com>
2024-03-05 11:06:16 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerBusiness.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("--- TEST 5: seller update ---");
+		seller = ((SellerBusiness)sellerBusiness).findById(1);
+		seller.setName("William Carter");
+		sellerBusiness.update(seller);
+		System.out.println("Update Completed");
 	}
 
 }
